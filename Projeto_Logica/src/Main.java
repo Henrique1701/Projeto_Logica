5205583795b699f6188a2da7fd752f23ef6edcb7
Henrique1701 <48698947+Henrique1701@users.noreply.github.com>
2019-10-14 16:59:46 -0300

    Deu merda
    O método da FNC esta errado...
    :(

--- Projeto_Logica/src/Main.java
+++ Projeto_Logica/src/Main.java
@@ -25,30 +25,91 @@ class Funcoes{
                     } else if (proposicao.charAt(i+1) != ')' && proposicao.charAt(i+1) != 'V'){
                         res = false;
                     }
-                } //CONTINUAR AQUI !!!!
+                } else if (proposicao.charAt(i) == 'V' ){
+                    if(proposicao.charAt(i-1) != 'P' && proposicao.charAt(i-1) != 'Q' && proposicao.charAt(i-1) != 'P' && proposicao.charAt(i-1) != 'P' ){
+                        res = false;
+                    } else if (proposicao.charAt(i+1) != 'P' && proposicao.charAt(i+1) != 'Q' && proposicao.charAt(i+1) != 'R' && proposicao.charAt(i+1) != 'S'){
+                        res = false;
+                    }
+                } else if (proposicao.charAt(i) == '~' ){
+                    if(proposicao.charAt(i-1) != 'P' && proposicao.charAt(i-1) != 'Q' && proposicao.charAt(i-1) != 'R' && proposicao.charAt(i-1) != 'S' &&
+                            proposicao.charAt(i-1) != '(' && proposicao.charAt(i-1) != 'V'){
+                        res = false;
+                    } else if (proposicao.charAt(i+1) != 'P' && proposicao.charAt(i+1) != 'Q' && proposicao.charAt(i+1) != 'R' && proposicao.charAt(i+1) != 'S'){
+                        res = false;
+                    }
+                } else if (proposicao.charAt(i) == '('){
+                    if (proposicao.charAt(i-1) != '&' ){
+                        res = false;
+                    } else if (proposicao.charAt(i+1) != 'P' && proposicao.charAt(i+1) != 'Q' && proposicao.charAt(i+1) != 'R' && proposicao.charAt(i+1) != 'S' &&
+                            proposicao.charAt(i+1) != '~'){
+                        res = false;
+                    }
+                } else if (proposicao.charAt(i) == ')'){
+                    if (proposicao.charAt(i-1) != 'P' && proposicao.charAt(i-1) != 'Q' && proposicao.charAt(i-1) != 'R' && proposicao.charAt(i-1) != 'S' &&
+                            proposicao.charAt(i-1) != ')'){
+                        res = false;
+                    } else if (proposicao.charAt(i+1) != ')' && proposicao.charAt(i+1) != '&' && proposicao.charAt(i+1) != 'V'){
+                        res = false;
+                    }
+                }
+            }
+
+            if(res == false){
+                i = tamanho;
             }
         }
 
         return res;
     }
 
+    public boolean Horn(String[] clausulas){
+        boolean res = true;
+
+
+
+        return res;
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Funcoes funcoes = new Funcoes();
 
-        String teste  = "Eu nao sei o que digitar"; //Colocar a entrada para retirar os espacos.
-        String [] aux = teste.split(" "); //Cria um array de string pegando as palavras separas por espaco
-        teste = "";
-        for(int i = 0; i < aux.length; i++){
-            teste += aux[i];
+        int quantidade = in.nextInt();
+
+        for (int k = 1; k < quantidade+1; k++) {
+            String proposicao = in.nextLine();
+            String[] propAux = proposicao.split(" ");
+            proposicao = "";
+            for (int i = 0; i < propAux.length; i++) {
+                proposicao += propAux[i];
+            }
+
+            System.out.println(funcoes.FNC(proposicao));
+
+            /*System.out.println("Problema #"+k);
+            if(funcoes.FNC(proposicao) == false){
+                System.out.println("Não está na FNC.");
+            } else {
+                System.out.println("Está na FNC.");
+            }
+            System.out.println();*/
         }
 
-        System.out.println(teste);
 
 
-        /*Estava fazendo o metodo FNC da classe Funcoes
-        * Mais Especificamente avaliando quando fosse V */
+        /*String aux = in.nextLine();
+        String[] aux2 = aux.split(" ");
+        aux = "";
+        for(int i = 0; i < aux2.length; i++){
+            aux += aux2[i];
+        }
+        String[] aux3 = aux.split("&");
+        System.out.println();*/
+
+        /*O metodo da FNC esta errado...
+         * :( */
     }
 }
